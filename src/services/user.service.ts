afe0d5b1f649b19705c8cfb9d1c3499b247fc5c0
thaison65 <dangthaison8354@gmail.com>
2025-06-24 14:01:29 +0700

    feat: add server get user by username

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,4 +6,6 @@ export const getUsers = () => userModel.getAllUsers();
 
 export const getUserById = (id: string) => userModel.getUserById(id);
 
+export const getUserByUsername = (username: string) => userModel.getUserByUsername(username);
+
 export const createUser = (data: UserType) => userModel.createUser(data);
