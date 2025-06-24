ccdb4303d31e594e06034406a73ae8eb092106c2
thaison65 <dangthaison8354@gmail.com>
2025-06-24 14:00:43 +0700

    feat: add check Middleware Auth

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { getAll, getById, create } from '../controllers/user.controller';
+import { authMiddleware } from '../middlewares';
+import { validateInputMiddleware } from '../middlewares/validateInputMiddleware';
 
 const router = Router();
 
-router.get('/', getAll);
-router.get('/:id', getById);
-router.post('/', create);
+router.get('/', authMiddleware, getAll);
+router.get('/:id', validateInputMiddleware, authMiddleware, getById);
+router.post('/', validateInputMiddleware, authMiddleware, create);
 
 export default router;
